fee6dd828d3e050ed2bd54f733228297f1c2d58e
Vipul Singh <120162007+VIPULSINGH18@users.noreply.github.com>
2025-04-24 18:37:03 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -31,3 +31,197 @@ CREATE SCHEMA silver;
 GO
 CREATE SCHEMA gold;
 GO
+
+
+
+
+
+/* before creating the table we have to check whether the table exists or not */
+
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+/* using DDL commands and creating tables of source crm and source erp */
+
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_material_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
+
+
+IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_date DATETIME,
+prd_end_date DATETIME
+);
+
+
+IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+
+
+IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
+
+
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
+
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50), 
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
+
+
+
+
+/* now we have created the table but it's empty so we have to load the data inside the table...
+But before loading the data we have to write TRUNCATE function, assuming if already data is loaded
+in past and if we will load the data again then there will be duplicacy of data*/
+
+
+
+TRUNCATE TABLE bronze.crm_cust_info;
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_crm\cust_info.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.crm_cust_info        /*displaying the data */
+SELECT COUNT(*) FROM bronze.crm_cust_info     /*count of rows */
+
+
+
+
+
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_crm\prd_info.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.crm_prd_info        /*displaying the data */
+SELECT COUNT(*) FROM bronze.crm_prd_info     /*count of rows */
+
+
+
+
+
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_crm\sales_details.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.crm_sales_details       /*displaying the data */
+SELECT COUNT(*) FROM bronze.crm_sales_details     /*count of rows */
+
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_erp\cust_az12.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.erp_cust_az12        /*displaying the data */
+SELECT COUNT(*) FROM bronze.erp_cust_az12     /*count of rows */
+
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_erp\loc_a101.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.erp_loc_a101        /*displaying the data */
+SELECT COUNT(*) FROM bronze.erp_loc_a101     /*count of rows */
+
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\HP\OneDrive\Desktop\data warehouse project\source_erp\px_cat_g1v2.csv'
+WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+);
+
+
+SELECT * FROM bronze.erp_px_cat_g1v2        /*displaying the data */
+SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2     /*count of rows */
